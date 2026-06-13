7d5359aa097768ccae677d2e31e675b9446d3fbf
AFilipino <156818046+AFilipino@users.noreply.github.com>
2026-06-13 21:02:23 +0800

    ui started

--- Prototype/Assets/Scripts/Player.cs
+++ Prototype/Assets/Scripts/Player.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Stats : MonoBehaviour
+public class Player : MonoBehaviour
 {
     [SerializeField] public string Name;
     [SerializeField] public int MaxHP;
@@ -9,7 +9,5 @@ public class Stats : MonoBehaviour
     [SerializeField] public int Speed;
     //[SerializeField] public int PositionX;
     //[SerializeField] public int PositionY;
-
-    
 
 }
