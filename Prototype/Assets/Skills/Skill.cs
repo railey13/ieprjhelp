a6bbed7bbefce06d7d491f92bf44329e70b899a3
ValentinDomingoIII <valentin_domingoiii@dlsu.edu.ph>
2026-06-25 16:11:01 +0800

    sad

--- Prototype/Assets/Skills/Skill.cs
+++ Prototype/Assets/Skills/Skill.cs
@@ -4,6 +4,6 @@ public abstract class Skill : ScriptableObject
 {
     public string SkillName;
     public int MPCost;
-
+    public int range;
     public abstract void Use(UnitClass user, UnitClass target);
 }
