2157288297b2d6e9196a2ec1bb71b92143cadcd6
Enzo <enzo_liberato@dlsu.edu.ph>
2026-08-06 17:19:11 +0800

    Revert "Updated VaericUnit.cs"
    This reverts commit 38a49e71667ee77b297df0e74430770797c15735.

--- Prototype/Assets/Scripts/Characters/VaericUnit.cs
+++ Prototype/Assets/Scripts/Characters/VaericUnit.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class VaericUnit : PlayerClass, IOnSkillUsedPassive, IOutgoingDamageModifier
 {
+    public int maxHp = 0;
     public int selfDamagePerAction = 5;
 
     [Header("Revive")]
@@ -10,6 +11,12 @@ public class VaericUnit : PlayerClass, IOnSkillUsedPassive, IOutgoingDamageModif
     public float revivedDamageMultiplier = 1.3f;
 
     private bool revivedBoostActive = false;
+
+    private void Start()
+    {
+        if (maxHp <= 0)
+            maxHp = hp;
+    }
 
     public override void BasicAttack(EnemyClass target)
     {
@@ -59,10 +66,7 @@ public class VaericUnit : PlayerClass, IOnSkillUsedPassive, IOutgoingDamageModif
             {
                 hasRevived = true;
                 revivedBoostActive = true;
-
-                // Enzo changes: this uses maxHp from UnitClass so I dont make another maxHp variable here
                 hp = maxHp;
-
                 atk += reviveAtkBoost;
 
                 Debug.Log(UnitName + " revived once and got stronger");
