b692ed2113ca00b7fbd8c2652a2698b3f4ec733c
DayritRN <47627436+TheAmbush@users.noreply.github.com>
2026-08-06 20:06:27 +0800

    waow

--- Prototype/Assets/Scripts/PlayerMovement.cs
+++ Prototype/Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,7 @@ public class PlayerMovement : MonoBehaviour
 {
     //ANIMATION
     private Animator animator;
-
-
+    public static bool AnyPlayerMoving { get; private set; }
 
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 12f;
@@ -116,12 +115,14 @@ public class PlayerMovement : MonoBehaviour
 
         SetCircleVisible(true); // show circle during movement 
         moving = true;
+        AnyPlayerMoving = true;
     }
 
     void ConfirmMovement()
     {
         moving = false;
         SetCircleVisible(false);
+        AnyPlayerMoving = false;
 
         if (turnSystem != null)
             turnSystem.OnPlayerMoveConfirmed();
@@ -130,7 +131,6 @@ public class PlayerMovement : MonoBehaviour
     void HandleWASD()
     {
         Vector2 input = moveAction.ReadValue<Vector2>();
-
 
         //ANIMATION
 
@@ -151,9 +151,8 @@ public class PlayerMovement : MonoBehaviour
         camRight.y = 0f;
         camRight.Normalize();
 
-        if (input == Vector2.zero) return;
-        // movement
-        Vector3 proposed = transform.position + new Vector3(input.x, 0f, input.y).normalized * moveSpeed * Time.deltaTime;
+        Vector3 moveDir = (camForward * input.y + camRight * input.x).normalized;
+        Vector3 proposed = transform.position + moveDir * moveSpeed * Time.deltaTime;
 
         Vector3 offset = proposed - origin;
         offset.y = 0f;
@@ -201,6 +200,7 @@ public class PlayerMovement : MonoBehaviour
         {
             moving = false;
             SetCircleVisible(false);
+            AnyPlayerMoving = false;
             transform.position = origin; // reset position to original
 
             if (turnSystem != null)
