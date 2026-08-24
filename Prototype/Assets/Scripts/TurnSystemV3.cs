a80095f177faf428aa0b8ac854eaee038f93327c
ValentinDomingoIII <valentin_domingoiii@dlsu.edu.ph>
2026-08-24 21:22:41 +0800

    Forced end turn

--- Prototype/Assets/Scripts/TurnSystemV3.cs
+++ Prototype/Assets/Scripts/TurnSystemV3.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using UnityEngine.InputSystem;
 
 
 public class TurnBasedSystemV3 : MonoBehaviour
@@ -64,10 +64,28 @@ public class TurnBasedSystemV3 : MonoBehaviour
             return;
 
         if (waitingForPlayer)
+        {
+            Debug.Log("WAITING FOR PLAYER - CT STOPPED");
+            if (Keyboard.current.xKey.wasPressedThisFrame)
+            {
+                TestEndTurn();
+            }
+
             return;
+        }
 
         TickChargeTime();
     }
+
+    private void TestEndTurn()
+    {
+    Debug.Log("Skipped turn");
+
+        EndTurn();
+
+    }
+
+
 
     private void BuildUnitList()
     {
@@ -124,21 +142,6 @@ public class TurnBasedSystemV3 : MonoBehaviour
 
         currentUnit = null;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     ///////////// PLACEHOLDERS////////////
    
@@ -227,7 +230,7 @@ public class TurnBasedSystemV3 : MonoBehaviour
             }
         }
     }
-
+        
     private void SpawnEnemies()
     {
         enemies.Clear();
